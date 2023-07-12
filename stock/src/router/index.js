b8ec7a6c4ef6d35fc3586d9331463d4e2384c38b
minsung <als0318tjd@gmail.com>
2023-07-12 10:42:13 +0900

    feat. MyPage

--- stock/src/router/index.js
+++ stock/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 // 연결할 각 컴포넌트 import (src/views폴더 아래 컴포넌트들 생성해둠)
 import BoardView from "../views/BoardView.vue";
 import PostView from "../views/PostView.vue";
+import MyPageView from "@/views/MyPageView.vue";
 
 // 라우터 설계
 const routes = [
