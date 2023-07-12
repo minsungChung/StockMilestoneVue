06b4547f9e7208ae69856afe0f78f1b2e31263c8
minsung <als0318tjd@gmail.com>
2023-07-12 16:18:32 +0900

    드디어~~^^

--- stock/src/views/PostView.vue
+++ stock/src/views/PostView.vue
@@ -8,6 +8,16 @@
     <div id="title">
       진영님 왜 저한테는 stash를 안 알려주셨나요ㅠ
     </div>
+    <div id="content">
+        아니 진영님은 대체 어딜 가신거야
+        부르지도 않았는데;;;;
+    </div>
+
+    <button id="butt">댓글 보기</button>
+    <div id="comment">
+      <textarea>댓글을 입력하세요</textarea>
+      <button id="butt">댓글 달기</button>
+    </div>
 
     </div>
   </div>
@@ -21,5 +31,21 @@
   display: flex;
   justify-content: flex-end;
     margin-bottom: 15px;
+}
+#content{
+  margin-top: 30px;
+  height: 40%;
+}
+textarea{
+  border: 1px solid orange;
+  border-radius: 5px;
+  width: 84%;
+  height: 40px;
+  padding: 10px;
+}
+#comment{
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
 }
 </style>
