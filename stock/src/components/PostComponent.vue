03696504ba2349688ffe6496ee1ff7e73546178d
younna <nyc6709@gmail.com>
2023-07-12 15:11:50 +0900

    test

---
+++ stock/src/components/PostComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="post-box"></div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+#post-box {
+  border: 1px solid;
+  border-radius: 10px;
+  padding: 30px;
+  background-color: LightBlue;
+}
+</style>
