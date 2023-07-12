f9b16cb51d15188a223169023c41525a937b23f8
jiny798 <jyoung7988@naver.com>
2023-07-12 14:24:58 +0900

    feat

---
+++ stock/src/components/user/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="contents">
+    <div class="form-wrapper form-wrapper-sm">
+      <form @submit.prevent="submitForm" class="form">
+        <div>
+          <label for="nickname">id:</label>
+          <input id="nickname" type="text" v-model="nickname" />
+          <p class="validation-text">
+            <span class="warning" v-if="!isUsernameValid && nickname">
+              Please enter an nickname
+            </span>
+          </p>
+        </div>
+        <div>
+          <label for="password">pw:</label>
+          <input id="password" type="text" v-model="password" />
+        </div>
+
+        <button type="submit" class="btn">로그인</button>
+      </form>
+      <p class="log">{{ logMessage }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.btn {
+  color: white;
+}
+</style>
