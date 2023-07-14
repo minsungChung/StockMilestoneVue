821def1f3030db2313cc731c250a792086d6d802
jiny798 <jyoung7988@naver.com>
2023-07-14 11:38:04 +0900

    first commit

--- stock/src/api/auth.js
+++ stock/src/api/auth.js
@@ -1,11 +1,11 @@
 import { baseAxios } from "./index";
 
 function getSocialLoginLink(socialType) {
-    return baseAxios.get("/api/auth/" + socialType+"/link");
+  return baseAxios.get("/api/auth/" + socialType + "/link");
 }
 
-function getAccessToken(socialType,tokenRequest){
-    return baseAxios.post("/api/auth/" + socialType + "/token", tokenRequest)
+function getAccessToken(socialType, tokenRequest) {
+  return baseAxios.post("/api/auth/" + socialType + "/token", tokenRequest);
 }
 
 export { getSocialLoginLink, getAccessToken };
